1567697b6c78b2ddf9bcf3f12546a8f3b8888460
Daniel Calvin <arockne2003@yahoo.com>
2021-08-02 17:06:53 -0700

    Implement getToys

--- src/index.js
+++ src/index.js
@@ -12,4 +12,20 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  getToys();
 });
+
+//implement getToys
+  //fetches data from db
+  //for each toy in db 
+    //hold all info in a div with class of card
+      //h2 with toys name
+      //img with src
+      //p with likes
+      //btn with class like-btn
+        //id with toy attribute
+function getToys() {
+  fetch('http://localhost:3000/toys')
+  .then(resp => resp.json())
+  .then(toys => toys.forEach(toy => createToy(toy)));
+}
